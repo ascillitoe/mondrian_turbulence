3b26fd282149bb050a357d7fe7cd0b92cae8dbc9
Ashley Scillitoe <ashleyscillitoe@googlemail.com>
2019-07-31 19:20:56 +0100

    Precision-recall curves and scoring metrics finished

--- cfd2ml/classify.py
+++ cfd2ml/classify.py
@@ -9,8 +9,6 @@ import matplotlib.pyplot as plt
 
 def RF_classifier(X_data,Y_data,options=None):
     from sklearn.ensemble import RandomForestClassifier
-#        from sklearn.metrics import classification_report, confusion_matrix, precision_recall_curve
-#        from cfd2ml.utilities import print_cm
 
     ####################
     # Parse user options
@@ -19,6 +17,7 @@ def RF_classifier(X_data,Y_data,options=None):
     gridsearch = False
     accuracy = False
     cv_type = 'logo'
+    scoring = 'f1'
 
     if (options is not None):
 
@@ -34,22 +33,21 @@ def RF_classifier(X_data,Y_data,options=None):
         if (("accuracy" in options)==True):
             accuracy = True
             from sklearn.model_selection import cross_validate
+            from sklearn.metrics import precision_recall_curve, auc, f1_score, accuracy_score, balanced_accuracy_score, confusion_matrix
+            from cfd2ml.utilities import print_cm
 
-        if (("cross_validation" in options)==True):
-            cv_type = options["cross_validation"]
-
+        if (("scoring" in options)==True):
+            scoring = options['scoring']
 
     ##############
     # Prepare data
     ##############
-    #TODO - check for missing values and NaN's
-
     # Find feature and target headers
     X_headers = X_data.columns
     Y_header  = Y_data.name
- 
-    X_train = X_data.drop(columns='group')
-    Y_train = Y_data
+
+    if(cv_type=='logo'): groups = X_data['group']
+    X_data = X_data.drop(columns='group')
    
     nX = X_headers.size
     print('\nFeatures:')
@@ -63,81 +61,154 @@ def RF_classifier(X_data,Y_data,options=None):
     # Setting cross-validation type (either leave-one-group-out or 5-fold)
     if(cv_type=='logo'):
         from sklearn.model_selection import LeaveOneGroupOut
-        groups = X_data['group'].to_numpy()
-        ngroup = groups[-1]
+        logo = LeaveOneGroupOut()
+        ngroup = logo.get_n_splits(groups=groups)
         print('\nUsing Leave-One-Group-Out cross validation on ', ngroup, ' groups')
-        cv = LeaveOneGroupOut().split(X_train,Y_train,groups)
-    elif(gridsearch_type=='kfold'):
+    elif(cv_type=='kfold'):
+        from sklearn.model_selection import kfold
         print('\nUsing 5-fold cross validation')
         cv = 5
 
-   
     #########################
     # Training the classifier
     #########################
     # TODO TODO TODO - improve accuracy by using balanced or weighted random forest
     # (see https://statistics.berkeley.edu/sites/default/files/tech-reports/666.pdf)
     if(gridsearch==True):
-        # Define scoring to maximise when searching for optimal hyperparameters
-        scoring = 'f1' #TODO- use balanced accuracy or f1 here?
-
         # Optimal hyperparameters found with GridSearchCV
         print('\n Performing GridSearchCV to find optimal hyperparameters for random forest classifier')
         clf = RandomForestClassifier()
-        CV_clf = GridSearchCV(estimator=clf,param_grid=gridsearch_params, cv=cv, scoring=scoring, n_jobs=-1, iid=False, verbose=1)
-        CV_clf.fit(X_train,Y_train)
+        if (cv_type=='logo'): cv = logo.split(X_data,Y_data,groups)
+        GS_clf = GridSearchCV(estimator=clf,param_grid=gridsearch_params, cv=cv, scoring=scoring, n_jobs=-1, iid=False, verbose=1)
+        GS_clf.fit(X_data,Y_data)
         
-        best_params = CV_clf.best_params_
-        best_score  = CV_clf.best_score_
-        clf = CV_clf.best_estimator_
+        best_params = GS_clf.best_params_
+        best_score  = GS_clf.best_score_
+        clf = GS_clf.best_estimator_  # (this clf has been fit to all of the X_data,Y_data)
 
         print('\nBest hyperparameters found:', best_params)
         print('\nScore with these hyperparameters:', best_score)
 
-        #if(accuracy==True):
-            # TODO - Training and validation error versus hyper-parameters
 
     else:
         # Train RF classifier with hyperparameters given by user
         print('\nTraining random forest classifer with given hyperparameters')
         clf = RandomForestClassifier(**params)
-        clf.fit(X_train,Y_train)
+        clf.fit(X_data,Y_data)
 
-        # Cross validation accuracy metrics
-        if(accuracy==True):
-            # Define scoring metrics to measure during cross validation
-            scoring = ['f1','accuracy','balanced_accuracy']
+    # Cross validation accuracy metrics
+    if(accuracy==True):
+        print('\nPerforming cross validation to determine train and test accuracy/error, and precision-recall curves')
 
-            print('\nPerforming cross validation to determine train and test accuracy/error')
-            cv_results = cross_validate(clf,X_train,Y_train,cv=cv,return_train_score=True,scoring=scoring,verbose=1,n_jobs=-1)
-
-            train_f1 = cv_results['train_f1']
-            test_f1  = cv_results['test_f1']
-            train_A = cv_results['train_accuracy']
-            test_A  = cv_results['test_accuracy']
-            train_BA = cv_results['train_balanced_accuracy']
-            test_BA  = cv_results['test_balanced_accuracy']
-
-            train_TE = 1.0 - train_A
-            test_TE  = 1.0 - test_A
-            train_CAE = 1.0 - train_BA
-            test_CAE  = 1.0 - test_BA
-
-            print('\nTraining scores:')
-            print('F1 score = %.2f %%' %(np.mean(train_f1)*100) )
-            print('Total error = %.2f %%' %(np.mean(train_TE)*100) )
-            print('Per-class error = %.2f %%' %(np.mean(train_CAE)*100) )
-     
-            print('\nValidation scores:')
-            print('F1 score = %.2f %%' %(np.mean(test_f1)*100) )
-            print('Total error = %.2f %%' %(np.mean(test_TE)*100) )
-            print('Per-class error = %.2f %%' %(np.mean(test_CAE)*100) )
-
-        #TODO - precision-recall curves to decide threshold
-        #TODO - Need to add X_train, X_test, Y_train, Y_test = train_test_split(q_dataset[X_headers], e_dataset[Y_headers],train_size=train_percentage,random_state=random_state) again,
-        #       in order to use test data for pr curves? Or can build into CV?
         #TODO - capability to decide on probablity threshold, and predict with chosen threshold
-        # See https://stackoverflow.com/questions/29656550/how-to-plot-pr-curve-over-10-folds-of-cross-validation-in-scikit-learn
+
+        # Get generator object depending on cv strategy
+        if (cv_type=='logo'): 
+            cv = logo.split(X_data,Y_data,groups)
+        elif(cv_type=='kfold'):
+            k_fold = KFold(n_splits=cv)
+            k_fold.split(X_data)
+
+        fig1, ax1 = plt.subplots()
+
+        # Init lists
+        y_real   = []
+        y_proba  = []
+        train_f1 = []
+        test_f1  = []
+        train_A  = []
+        test_A   = []
+        train_BA = []
+        test_BA  = []
+
+        # Loop through CV folds
+        i = 0
+        for train_index, test_index in cv:
+            X_train, X_test = X_data.iloc[train_index], X_data.iloc[test_index]
+            Y_train, Y_test = Y_data.iloc[train_index], Y_data.iloc[test_index]
+
+            # Train classifier
+            clf_cv = clf
+            clf_cv.fit(X_train, Y_train)
+
+            # Predict Y
+            Y_pred_train = clf_cv.predict(X_train)
+            Y_pred_test  = clf_cv.predict(X_test )
+
+            # F1 scores
+            f1score = f1_score(Y_test , Y_pred_test)
+            train_f1.append(f1_score(Y_train, Y_pred_train) )
+            test_f1.append(f1score)
+            # Accuracy scores
+            Ascore = accuracy_score(Y_test , Y_pred_test)
+            train_A.append(accuracy_score(Y_train, Y_pred_train) )
+            test_A.append(Ascore)
+            # Balanced accuracy scores
+            BAscore = balanced_accuracy_score(Y_test , Y_pred_test)
+            train_BA.append(balanced_accuracy_score(Y_train, Y_pred_train) )
+            test_BA.append(BAscore)
+
+            # Print validation scores (training scores are stored to print mean later, but not printed for each fold)
+            if(cv_type=='logo'):
+                print('\nTest group = ', groups.iloc[test_index[0]])
+            elif(cv_type=='kfold'):
+                print('\nFold = ', i)
+            print('-------------------')
+            print('F1 score = %.2f %%' %(f1score*100) )
+            print('Total error = %.2f %%' %((1.0-Ascore)*100) )
+            print('Per-class error = %.2f %%' %((1.0-BAscore)*100) )
+
+            # Print confusion matrix for this fold
+            print('Confusion matrix:')
+            confuse_mat = confusion_matrix(Y_test, Y_pred_test)
+            print_cm(confuse_mat, ['Off','On'])
+            
+            # Prediction probability based on X_test (used for precision-recall curves)
+            pred_proba = clf_cv.predict_proba(X_test)
+            precision, recall, _ = precision_recall_curve(Y_test, pred_proba[:,1])
+            lab = 'Fold %d AUC=%.4f' % (i+1, auc(recall, precision))
+            ax1.step(recall, precision, label=lab)
+            y_real.append(Y_test)
+            y_proba.append(pred_proba[:,1])
+
+            i += 1
+
+        # Calculate errors from accuracies
+        train_TE = 1.0 -  np.array(train_A)
+        test_TE  = 1.0 -  np.array(test_A)
+        train_CAE = 1.0 - np.array(train_BA)
+        test_CAE  = 1.0 - np.array(test_BA)
+
+        # Print performance scores
+        print('\nMean training scores:')
+        print('F1 score = %.2f %%' %(np.mean(train_f1)*100) )
+        print('Total error = %.2f %%' %(np.mean(train_TE)*100) )
+        print('Per-class error = %.2f %%' %(np.mean(train_CAE)*100) )
+    
+        print('\nMean validation scores:')
+        print('F1 score = %.2f %%' %(np.mean(test_f1)*100) )
+        print('Total error = %.2f %%' %(np.mean(test_TE)*100) )
+        print('Per-class error = %.2f %%' %(np.mean(test_CAE)*100) )
+
+        # Average precision-recall over folds, and plot curves
+        y_real = np.concatenate(y_real)
+        y_proba = np.concatenate(y_proba)
+        precision, recall, _ = precision_recall_curve(y_real, y_proba)
+        lab = 'Overall AUC=%.4f' % (auc(recall, precision))
+        ax1.step(recall, precision, label=lab, lw=2, color='black')
+        ax1.set_xlabel('Recall')
+        ax1.set_ylabel('Precision')
+        ax1.legend(loc='lower left', fontsize='small')
+        
+
+# TODO - Training and validation error versus hyper-parameters if gridsearch
+
+        plt.show()
+
+    return clf
+
+
+
 #
 #
 #    else:
@@ -188,7 +259,6 @@ def RF_classifier(X_data,Y_data,options=None):
 #
 #    clf.verbose = True # reset
 
-    return clf
 
 def DT_classifier(q_dataset,e_dataset,accuracy=False,train_percentage=0.75,criterion='entropy',random_state=42,max_depth=5):
     from sklearn.model_selection import train_test_split
