precision-recall curve added for predict. Fix to confusion matrix. -ve Pk label added.

cfd2ml/preproc.py
@@ -483,7 +483,7 @@ def make_errors(les_vtk):
     les_dsa = dsa.WrapDataObject(les_vtk)
 
     print('Error metric:')
-    nerr = 2
+    nerr = 3
     err = 0
     e_raw  = np.zeros([les_nnode,nerr])
     e_bool = np.zeros([les_nnode,nerr],dtype=int)
@@ -556,11 +556,30 @@ def make_errors(les_vtk):
     #e_raw[:,1] = inv2
     e_raw[:,err] = inv3
     
-    #index = algs.where(inv2>1.0/6.0)   #TODO - study what is best to use here. inv2, inv3, c1c etc... 
-    index = algs.where(algs.abs(inv3)>0.01)
+#    index = algs.where(inv2>1.0/6.0)   #TODO - study what is best to use here. inv2, inv3, c1c etc...  # DATA1
+#    index = algs.where(algs.abs(inv3)>0.01)                                                            # DATA2
+    index = algs.where(algs.abs(inv3)>0.005)                                                           # DATA3
+#    index = algs.where((uiuj[:,0,0]/uiuj[:,2,2])>2.0)                                                   # DATA4
+#    index = algs.where((uiuj[:,1,1]/uiuj[:,2,2])>1.0)                                                   # DATA5
+
     e_bool[index,err] = 1
     error_labels[err] = 'Stress anisotropy'
     err += 1
+
+    # Error metric 2: Negative Pk
+    ############################################
+    A = np.zeros(les_nnode)
+    for i in range(0,3):
+        for j in range(0,3):
+            A[:] += (-uiuj[:,i,j] * J[:,i,j])
+
+    e_raw[:,err] = A
+    index = algs.where(A<0.0)
+
+    e_bool[index,err] = 1
+    error_labels[err] = 'Negative Pk'
+    err += 1
+
     
 #    # Error metric 3: Non-linearity
 #    ###############################

cfd2ml/utilities.py
@@ -99,18 +99,21 @@ def print_cm(cm, labels, hide_zeroes=False, hide_diagonal=False, hide_threshold=
         print("%{0}s".format(columnwidth) % label, end=" ")
     print()
     # Print rows
-    for i, label1 in enumerate(labels):
-        print("    %{0}s".format(columnwidth) % label1, end=" ")
-        for j in range(len(labels)):
-            cell = "%{0}.1d".format(columnwidth) % cm[i, j]
-            if hide_zeroes:
-                cell = cell if float(cm[i, j]) != 0 else empty_cell
-            if hide_diagonal:
-                cell = cell if i != j else empty_cell
-            if hide_threshold:
-                cell = cell if cm[i, j] > hide_threshold else empty_cell
-            print(cell, end=" ")
-        print()
+    if(np.shape(cm)==(1,1)):
+        print('No confusion!')
+    else:
+        for i, label1 in enumerate(labels):
+            print("    %{0}s".format(columnwidth) % label1, end=" ")
+            for j in range(len(labels)):
+                cell = "%{0}.1d".format(columnwidth) % cm[i, j]
+                if hide_zeroes:
+                    cell = cell if float(cm[i, j]) != 0 else empty_cell
+                if hide_diagonal:
+                    cell = cell if i != j else empty_cell
+                if hide_threshold:
+                    cell = cell if cm[i, j] > hide_threshold else empty_cell
+                print(cell, end=" ")
+            print()
 
 def netcdf_to_vtk(cdffile,cdfscalars,vtkscalars):
     import vista